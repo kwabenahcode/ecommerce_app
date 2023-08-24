e16d28bb799cf1a0e0e0588c42167bcf303261f1
kwabenahcode <eofori080@st.ug.edu.gh>
2023-08-24 00:00:24 +0000

    added routes

--- market.py
+++ market.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#Routes for the home Page
 @app.route('/')
-def hello():
-    return '<p>Hello world</p>'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+#
+
+# @app.route('/about/<user>')
+# def about(user):
+#     return f"<h1>This is the about page {user}'s</h1>"
 
