df3359e70f36275381670c8c30c889f569ac32a4
kwabenahcode <eofori080@st.ug.edu.gh>
2023-08-24 14:33:16 +0000

    created Columns to the item class

--- market.py
+++ market.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 # Created an instance of the Sqlalchemy
 db = SQLAlchemy(app)
 
+# Creates the various columns in the database
+class items(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    description = db.Column(db.String(length=1000), nullable=False, unique=True)
+
 #Routes for the home Page
 @app.route('/')
 @app.route('/home')
