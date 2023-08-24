ac5b8d2610fefa73ab64515a61d85458f8486b39
kwabenahcode <eofori080@st.ug.edu.gh>
2023-08-24 00:42:29 +0000

    added style

--- market.py
+++ market.py
@@ -8,7 +8,15 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
-#
+#Routes for the market Page
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '10923044', 'price': 500 },
+        {'id': 2, 'name': 'Laptop', 'barcode': '10906101', 'price': 900},
+        {'id': 3, 'name': 'keyboard', 'barcode': '10903933', 'price': 200}
+        ]
+    return render_template('market.html', items=items)
 
 # @app.route('/about/<user>')
 # def about(user):
