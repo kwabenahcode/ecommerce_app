5797b0cf7450bfd5bec9ddaf9a38e6388af0b96d
kwabenahcode <eofori080@st.ug.edu.gh>
2023-08-27 15:41:59 +0000

    created database relationship

--- market/models.py
+++ market/models.py
@@ -7,7 +7,8 @@ class users(db.Model):
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email = db.Column(db.String(length=50), nullable=False, unique=True)
     password = db.Column(db.String(length=20), nullable=False, unique=True)
-    
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('items', backref='owned_user', lazy=True)
     
 # Creates the various columns for items in the database
 class items(db.Model):
@@ -16,6 +17,8 @@ class items(db.Model):
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     description = db.Column(db.String(length=1000), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    
     
     def __repr__(self):
         return f'Item: {self.name}'
