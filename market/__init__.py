d387c1993e20cd0e605100d1011dfed1ebc035f5
kwabenahcode <eofori080@st.ug.edu.gh>
2023-08-28 22:35:06 +0000

    database modified

--- market/__init__.py
+++ market/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, current_app
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 
@@ -8,6 +10,13 @@ db = SQLAlchemy()
 
 # Sqlalchemy connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+engine = create_engine("mysql+pymysql://user:pw@host/db", pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+db.session.expire_on_commit = False
 db.init_app(app)
 
 from market import routes 
+
+# with app.app_context():
+#     db.create_all()
--- market/models.py
+++ market/models.py
@@ -3,21 +3,21 @@ from market import db
 
 #creating database for users
 class users(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(length=30), nullable=False, unique=True)
-    email = db.Column(db.String(length=50), nullable=False, unique=True)
-    password = db.Column(db.String(length=20), nullable=False, unique=True)
-    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(20), nullable=False, unique=True)
+    budget = db.Column(db.Integer, nullable=False, default=1000)
     items = db.relationship('items', backref='owned_user', lazy=True)
     
 # Creates the various columns for items in the database
 class items(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30), nullable=False, unique=True)
-    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    price = db.Column(db.Integer(), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    barcode = db.Column(db.String(12), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(length=1000), nullable=False, unique=True)
-    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    owner = db.Column(db.Integer, db.ForeignKey('users.id'))
     
     
     def __repr__(self):
