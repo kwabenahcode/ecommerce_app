a4910802db89f0106693c6349f6abca43e02cbf6
kwabenahcode <eofori080@st.ug.edu.gh>
2023-08-29 15:24:08 +0000

    adding validation to rgister form

--- market/forms.py
+++ market/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name')
+    username = StringField(label='User Name', validators='Length(min=2, max=)')
     email = StringField(label='Email Address')
     password1 = PasswordField(label='Password')
     password2 = PasswordField(label='Confirm Password')
