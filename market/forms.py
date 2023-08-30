b144e56f8569dda76b18c422a0ead1973de8125e
kwabenahcode <eofori080@st.ug.edu.gh>
2023-08-30 13:45:25 +0000

    modified erorr messages

--- market/forms.py
+++ market/forms.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo,Email, DataRequired
+from wtforms.validators import Length, EqualTo,Email, DataRequired, ValidationError
+from market.models import users
 
 class RegisterForm(FlaskForm):
+    
+    def validate_username(self, username_to_check):
+        user_exist = users.query.filter_by(username=username_to_check).first()
+        if user_exist:
+            raise ValidationError("There username already exist")
+        
+    def validate_email(self, email_to_check):
+        email_exist = users.query.filter_by(email=email_to_check).first()
+        if email_exist:
+            raise ValidationError("The email already exist")
+        
     username = StringField(label='User Name', validators=[Length(min=2, max=30), DataRequired()])
     email = StringField(label='Email Address', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
